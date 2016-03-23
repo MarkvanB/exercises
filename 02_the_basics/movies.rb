d9011d0a278bf15c1a19999787ab5fea1cc7128d
Mark van Brecht <m.van.brecht@xlsglobal.com>
2016-03-23 20:39:35 +0100

    all files so far

---
+++ 02_the_basics/movies.rb
@@ -0,0 +1,9 @@
+Movie = { the_matrix: 1999,
+          the_godfather: 1977,
+          jurassic_park: 1995,
+          armageddon: 2001}
+
+puts Movie[:the_matrix]
+puts Movie[:the_godfather]
+puts Movie[:jurassic_park]
+puts Movie[:armageddon]
